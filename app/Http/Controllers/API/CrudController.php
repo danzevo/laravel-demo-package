2d35a8e9937b16f708af4962f352ca4bb7ac4c04
dani nugrahadi pradana <dani.nugrahadi@gmail.com>
2022-01-15 13:46:41 +0700

    first commit

---
+++ app/Http/Controllers/API/CrudController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Jwalbeli\Crudapi\Crudapi;
+
+class CrudController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $crudapi = new Crudapi();
+        return $crudapi->index();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $crudapi = new Crudapi();
+        return $crudapi->store($request);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $crudapi = new Crudapi();
+        return $crudapi->update($request);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $crudapi = new Crudapi();
+        return $crudapi->destroy($id);
+    }
+}
